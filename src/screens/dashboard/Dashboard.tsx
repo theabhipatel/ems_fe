0c8a4cf12c0d8071e7d3d401d86b103caa216f87
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-07-14 17:40:21 +0530

    chore used custom button component in dashboard screen

--- src/screens/dashboard/Dashboard.tsx
+++ src/screens/dashboard/Dashboard.tsx
@@ -1,21 +1,43 @@
+import Button from "@/components/Button";
+
 const Dashboard = () => {
   return (
-    <div className="w-full h-[70vh] flex justify-between items-center p-10 bg-gray-300">
-      <div className="w-80 p-3 rounded-md bg-light-background border-light-border">
-        <h1 className="text-xl font-semibold text-light-textPrimary ">This is the heading.</h1>
-        <p className="text-light-textSecondary">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, voluptates ut ipsum, nisi tempora architecto
-          ad nulla laborum culpa quam iusto, ea accusamus doloremque?
-        </p>
-        <button className="px-3 py-1 bg-light-accentPrimary text-white rounded-md my-3 float-end">See More</button>
-      </div>
-      <div className="w-80 p-3 rounded-md bg-dark-background border-dark-border">
-        <h1 className="text-xl font-semibold text-dark-textPrimary ">This is the heading.</h1>
-        <p className="text-dark-textSecondary">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, voluptates ut ipsum, nisi tempora architecto
-          ad nulla laborum culpa quam iusto, ea accusamus doloremque?
-        </p>
-        <button className="px-3 py-1 bg-light-accentPrimary text-white rounded-md my-3 float-end">See More</button>
+    <div className="w-full h-screen flex justify-between items-center p-20 bg-black">
+      <div>
+        <div className="flex items-center  bg-light-accentSecondary/5 text-light-accentSecondary border-[1.25px] border-light-accentSecondary/45 px-4 py-2 rounded-md mt-2">
+          <span className="mr-3">#</span>
+          <p className="text-xs">You will need admin privileges to install and access this application.</p>
+        </div>
+        <div className="flex items-center bg-light-accentPrimary/5 text-light-accentPrimary border-[1.25px] border-light-accentPrimary/50 px-4 py-2 rounded-md mt-2">
+          <span className="mr-3">#</span>
+          <p className="text-xs"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, tempora!</p>
+        </div>
+        <button className="flex items-center bg-light-error/5 text-light-error/70 border-[1.25px] border-light-error/30 px-4 py-2 rounded-md mt-2">
+          <span className="mr-3">#</span>
+          <p className="text-xs"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, tempora!</p>
+          <div></div>
+        </button>
+        <Button>
+          <span className="mr-3">#</span>
+          <p className="text-xs"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, tempora!</p>
+        </Button>
+        <Button buttonColor="green">
+          <span className="mr-3">#</span>
+          <p className="text-xs"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, tempora!</p>
+        </Button>
+        <Button buttonColor="blue">
+          <span className="mr-3">#</span>
+          <p className="text-xs"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, tempora!</p>
+        </Button>
+        <Button buttonColor="red" className="flex flex-col items-start">
+          <div className="flex flex-col items-start text-start">
+            <span className="mr-3"># Something Heading</span>
+            <p className="text-xs"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, tempora!</p>
+            <p className="text-xs">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, tempora! Lorem ipsum dolor sit amet
+            </p>
+          </div>
+        </Button>
       </div>
     </div>
   );
